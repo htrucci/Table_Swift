d64c59dcaade46a8c0433ccf70214942cde766b8
htrucci <htrucci@gmail.com>
2017-06-25 13:49:11 +0900

    Initial Commit

---
+++ Table_SwiftTests/Table_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Table_SwiftTests.swift
+//  Table_SwiftTests
+//
+//  Created by Htrucci on 2017. 6. 25..
+//  Copyright © 2017년 Htrucci. All rights reserved.
+//
+
+import XCTest
+@testable import Table_Swift
+
+class Table_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
